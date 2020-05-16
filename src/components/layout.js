b527d88595d5927a93549ae527155282614772de
dimitriktm <dimitri32023@gmail.com>
2020-05-16 14:26:12 +0200

    _

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,7 +30,65 @@ const Layout = ({ children }) => {
       <CDN />
       <div>
         <main>{children}</main>
-        <footer></footer>
+        <footer>
+          <div>
+            <h4>Our services</h4>
+            <div className="line"></div>
+            <ul>
+              <li>
+                <a href="#">Web development</a>
+              </li>
+              <li>
+                <a href="#">Mobile development</a>
+              </li>
+              <li>
+                <a href="#">Cloud Technologies</a>
+              </li>
+              <li>
+                <a href="#">UI/UX Design</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4>Our links</h4>
+            <div className="line"></div>
+
+            <ul>
+              <li>
+                <a href="#">Terms & Conditions</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Imprint</a>
+              </li>
+              <li>
+                <a href="#">Legal</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4>Contact Us</h4>
+            <div className="line"></div>
+            <ul>
+              <li>
+                <a href="#">
+                  Al. Dummyodl 124/23 floor 123 Lipsum Street, 02-577 USA.
+                </a>
+              </li>
+              <li>
+                <a href="#">00 387 65 302 657</a>
+              </li>
+              <li>
+                <a href="#">Imprint</a>
+              </li>
+              <li>
+                <a href="#">mail@mail.com</a>
+              </li>
+            </ul>
+          </div>
+        </footer>
       </div>
     </>
   )
