34a0390625d4a384d442bb66dd27e33ed08ecfca
dimitriktm <dimitri32023@gmail.com>
2020-05-19 00:10:31 +0200

    _

--- src/components/btn-with-opacity/btn-with-opacity.js
+++ src/components/btn-with-opacity/btn-with-opacity.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Button from "react-bootstrap/Button"
 import "./btn-with-opacity.scss"
+/*** I hate naming things ******* @error */
+
 export default ({ className = "", children, ...props }) => (
     <Button
         {...props}
